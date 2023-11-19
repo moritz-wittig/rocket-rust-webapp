a09ae7745ef33d1f6b38d5807a51a329dc43f8d0
moritz-wittig <wittigmoritz@icloud.com>
2023-11-19 16:25:44 +0100

    Add: Hello World

--- rust_rocket_webapp/src/main.rs
+++ rust_rocket_webapp/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String{
+    String::from("Hello World")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
