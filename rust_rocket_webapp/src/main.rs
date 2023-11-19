2e6a245e9b7a5635ba726cd99bbc3b211b510828
moritz-wittig <wittigmoritz@icloud.com>
2023-11-19 17:34:17 +0100

    Add: Making API requests inside rocket server (Example not working yet)

--- rust_rocket_webapp/src/main.rs
+++ rust_rocket_webapp/src/main.rs
@@ -1,5 +1,7 @@
-use rocket::{http::{Status, uri::Origin}, response::Redirect, serde::json::{Value, json}};
-
+use rocket::{http::{Status, uri::Origin}, response::Redirect, serde::json::{Value, json }};
+use rocket::State;
+use reqwest;
+use reqwest::Client;
 
 #[macro_use]
 extern crate rocket;
@@ -12,22 +14,33 @@ fn index() -> Redirect{
     Redirect::to(uri!(TAURI_RELEASES_PREFIX, google_keep_desktop_api("windows-x86_64", "v1.0.14", msg)))
 }
 
+const GOOGLE_KEEP_DESKTOP_REPO: &str = "elibroftw/google-keep-desktop-app";
+
+async fn get_latest_release(client: &State<Client>, repo: &str) -> Result<Value, reqwest::Error>{
+    let url = format!("https://api.github.com/repos/{repo}/releases/latest");
+    let response = client.get(&url).send().await?;
+    let github_release = response.json::<Value>().await?;
+    Ok(github_release)
+}
+
 #[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
-fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>) -> Result<Value, Status>{
+async fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>, client: &State<  Client>) -> Result<Value, Status>{
     // Status::NoContent
     if let Some(msg) = msg{
         println!("{msg}");
         return Err(Status::NoContent);
     }
-
-    Ok(json!({
-        "notes": "IT WORKS"
-    }))
+    get_latest_release(client, GOOGLE_KEEP_DESKTOP_REPO).await.or (Err(Status::NoContent))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .manage(reqwest::Client::builder()
+            .user_agent("reqwest")
+            .build()
+            .unwrap()
+        )
         .mount("/", routes![index])
         .mount(TAURI_RELEASES_PREFIX, routes![google_keep_desktop_api])
 }
